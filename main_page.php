9653797ea874c81ec01c5d2a56881892997a5671
Anthony Lackey <alackey96@gmail.com>
2015-01-30 18:38:51 -0800

    Add website files

---
+++ main_page.php
@@ -0,0 +1,50 @@
+<html>
+<head>
+	<title>Anthony Lackey</title>
+	<link rel="stylesheet" type="text/css" href="main_page.css">
+</head>
+
+<body>
+	<nav class="nav">
+		<ul class="nav_left">
+			<li><a href="main_page.php"> Anthony Lackey</a></li>
+		</ul>
+		<ul class="nav_right">
+			<li><a href="main_page.php">Home</a></li>
+			<li><a href="main_page.php">Resume</a></li>
+			<li><a href="https://github.com/Alackey/">Projects</a></li>
+			<li><a href="main_page.php">Contact</a></li>
+		</ul>
+	</nav>
+	<div class="middle">
+		<div class="middle_container">
+			<div class="left_bracket bracket">
+				<img src="left_bracket.png">
+			</div>
+			<div class="middle_content">
+				<img src="me_photo.jpg">
+				<h1>Anthony Lackey</h1>
+			</div>
+			<div class="right_bracket bracket">
+				<img src="right_bracket.png">
+			</div>
+		</div>
+	</div>
+	<nav class="footer">
+		<ul class="footer_email">
+			<li>Email:</li>
+			<li><a href="main_page.php">alackey96@gmail.com</a></li>
+		</ul>
+		<ul class="footer_links">
+			<li><a href="main_page.php">Home</a></li>
+			<li><a href="main_page.php">Resume</a></li>
+			<li><a href="https://github.com/Alackey/">Projects</a></li>
+			<li><a href="main_page.php">Contact</a></li>
+		</ul>
+		<ul class="footer_copyright">
+			<li><a href="main_page.php">&copy 2015 Anthony Lackey</a></li>
+		</ul>
+	</nav>
+</body>
+
+</html>
