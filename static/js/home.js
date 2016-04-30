d605943ec008d0b9bd368594cfc5b901c5b76861
Anthony Lackey <alackey96@gmail.com>
2016-04-30 00:45:33 -0700

    Skills: Add slide in on waypoint views

--- static/js/home.js
+++ static/js/home.js
@@ -13,4 +13,24 @@ $(document).ready(function() {
         offset: 'bottom-in-view'
     });
 
+    var wp_skillsRow1 = new Waypoint({
+        element: document.getElementById('skillsRow1'),
+        handler: function(direction) {
+            $("#skillsRow1").addClass("animated slideInDown");
+            $("#skillsRow1").css("opacity", "1");
+            this.destroy();
+        },
+        offset: 'bottom-in-view'
+    });
+
+    var wp_skillsRow1 = new Waypoint({
+        element: document.getElementById('skillsRow2'),
+        handler: function(direction) {
+            $("#skillsRow2").addClass("animated slideInUp");
+            $("#skillsRow2").css("opacity", "1");
+            this.destroy();
+        },
+        offset: 'bottom-in-view'
+    });
+
 });
